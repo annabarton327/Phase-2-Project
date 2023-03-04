cdca93c08295d36e597c983d0d4c6f5dd236734a
Anna Barton <anna.barton327@gmail.com>
2023-03-04 12:54:58 -0700

    new

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from "react"
 function Home() {
     return (
         <body>
-            <h1>EasyVacay</h1>
+            <h1>Vacay with Ease</h1>
             <h2>The vacation planner that just does it for you.</h2>
             <h3>Start your vacation before your vacation.</h3>
             <form>
--- src/components/MyTrip.jsx
+++ src/components/MyTrip.jsx
@@ -1,11 +1,16 @@
 import React from "react"
+import { Link } from "react-router-dom"
+
 
 function MyTrip() {
 
     return (
         <body>
-        <h1>You have no selected trips. Get started and begin now!</h1>
-        <button>Begin Trip</button>
+            <h1>My Trip</h1>
+            <p>Your trip is empty. Get started and begin your trip now!</p>
+            <button>
+                <Link to="/">Begin Trip</Link>
+            </button>
         </body>
     )
 }
